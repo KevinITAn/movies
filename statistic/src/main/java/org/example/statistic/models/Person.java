b83e985272e1dfba5480baddf9a463a864e72fbe
kevinn <kevincrocen@gmail.com>
2025-02-24 14:01:56 +0100

    bozza statisctic

--- statistic/src/main/java/org/example/statistic/models/Person.java
+++ statistic/src/main/java/org/example/statistic/models/Person.java
@@ -3,4 +3,9 @@ package org.example.statistic.models;
 public class Person {
     private String name;
     private String surname;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
 }
